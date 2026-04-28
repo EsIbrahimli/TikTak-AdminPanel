3d4b4d491645d7c9ee3ad2230ef3599164ba59a2
matanatyusibova <yusibovamatanat@gmail.com>
2026-04-29 02:10:26 +0400

    Added search bar

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,10 +1,30 @@
 import styles from './Header.module.css'
+import { useSearchStore } from '../../store/useSearchStore';
 
-const Header = () => {
+
+  const Header = () => {
+  const { query, results, setQuery, searchProducts } = useSearchStore();
   return (
     <header className={styles.header}>
         <h1 className={styles.logo} >TIK TAK ADMİN</h1>
-        <input className={styles.input} type="text" placeholder="Axtarış"></input>
+        <input
+          className={styles.input}
+          type="text"
+          placeholder="Axtarış"
+          value={query}
+          onChange={(e) => {
+            const value = e.target.value;
+            setQuery(value);
+            searchProducts(value);
+          }}
+        />
+         <ul>
+        {results.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.price}
+          </li>
+        ))}
+      </ul>
     </header>
   )
 }
